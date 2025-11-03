e392ed0b045fe486594313eb23dec2f2d24813da
Antonio Junior <juniorleao20@gmail.com>
2025-11-03 20:55:58 -0300

    feat: analisador semantico

---
+++ src/semantic_analyzer.h
@@ -0,0 +1,23 @@
+#ifndef SEMANTIC_ANALYZER_H
+#define SEMANTIC_ANALYZER_H
+
+#include "ast_base.h"
+#include "symbol_table.h"
+
+// Enum para seus tipos básicos
+typedef enum {
+    TYPE_INT,
+    TYPE_FLOAT,
+    TYPE_BOOL,
+    TYPE_STRING,
+    TYPE_VOID,
+    TYPE_ERROR // Um tipo especial para propagar erros
+} Type;
+
+/* * A função principal.
+ * Ela "visita" a AST, preenche a tabela de símbolos e checa os tipos.
+ * Retorna 1 (true) se tudo estiver correto, 0 (false) se houver erros.
+ */
+int check_semantics(ASTNode* ast_root, SymbolTable* table);
+
+#endif
